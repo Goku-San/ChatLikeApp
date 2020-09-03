08ac9cbffb85b714b0280b26dfb61774b0ab736f
Goku-San <gokusan838@gmail.com>
2020-09-03 13:57:57 +0200

    Added error_notifications to room form, tested RoomsController and RoomMessagesController create action

--- test/controllers/room_messages_controller_test.rb
+++ test/controllers/room_messages_controller_test.rb
@@ -1,7 +1,34 @@
 require 'test_helper'
 
 class RoomMessagesControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @goku = users :goku
+    @room = rooms :lobby
+  end
+
+  test "should handle invalid message" do
+    sign_in @goku
+
+    assert_no_difference 'RoomMessage.count' do
+      post room_messages_path,
+           params: { room_message: { message: "", room_id: @room.id } },
+           xhr:    true
+    end
+
+    assert_response :unprocessable_entity
+
+    assert_equal "can't be blank", response.parsed_body["errors"]["message"].join
+  end
+
+  test "should create valid message" do
+    sign_in @goku
+
+    assert_difference 'RoomMessage.count', 1 do
+      post room_messages_path,
+           params: { room_message: { message: "Test message", room_id: @room.id } },
+           xhr:    true
+    end
+
+    assert_response :success
+  end
 end
--- test/controllers/rooms_controller_test.rb
+++ test/controllers/rooms_controller_test.rb
@@ -19,11 +19,43 @@ class RoomsControllerTest < ActionDispatch::IntegrationTest
     assert_template 'rooms/index'
   end
 
+  # This test ensures that -- before_action authenticate_user! -- works properly
   test "should check authenticate user before_action" do
     get new_room_path
+
+    assert_response :redirect
 
     follow_redirect!
 
     assert_template 'sessions/new'
+  end
+
+  test "room create action" do
+    sign_in @goku
+
+    # invalid submission
+    assert_no_difference 'Room.count' do
+      post rooms_path, params: { room: { name: "" } }
+    end
+
+    assert_response :success
+
+    assert_template 'rooms/new'
+
+    assert_select 'div.alert-danger'
+
+    # valid submission
+    assert_difference 'Room.count', 1 do
+      post rooms_path, params: { room: { name: "Test room" } }
+    end
+
+    assert_response :redirect
+
+    follow_redirect!
+
+    assert_template "rooms/index"
+
+    assert_select 'a[href=?]', new_room_path, count: 1
+    assert_select 'div.alert-primary'
   end
 end
